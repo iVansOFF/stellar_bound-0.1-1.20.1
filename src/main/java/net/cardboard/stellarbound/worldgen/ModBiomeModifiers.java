1ac257d70d3d0bf1efa3cabdc8869dcb02bb91a1
Cardboard <handerdarkcosmic@gmail.com>
2026-02-03 15:20:55 -0500

    Items More Block 1 Entity Block 3 Mobs 1 Gun 1 Mana Weapon
    Texts Gradients
    Mana System
    Loot Tables
    
    Base

--- src/main/java/net/cardboard/stellarbound/worldgen/ModBiomeModifiers.java
+++ src/main/java/net/cardboard/stellarbound/worldgen/ModBiomeModifiers.java
@@ -1,6 +1,7 @@
 package net.cardboard.stellarbound.worldgen;
 
 import net.cardboard.stellarbound.Stellarbound;
+import net.cardboard.stellarbound.worldgen.biome.ModBiomes;
 import net.minecraft.core.HolderSet;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.data.worldgen.BootstapContext;
@@ -13,26 +14,64 @@ import net.minecraftforge.common.world.ForgeBiomeModifiers;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModBiomeModifiers {
-    public static final ResourceKey<BiomeModifier> ADD_MOONSTONE_ORE = registerKey("add_moonstone_ore");
-    public static final ResourceKey<BiomeModifier> ADD_SUNSTONE_ORE = registerKey("add_sunstone_ore");
+
+    public static final ResourceKey<BiomeModifier> ADD_OVERWORLD_MOONSTONE =
+            createKey("add_overworld_moonstone");
+
+    public static final ResourceKey<BiomeModifier> ADD_OVERWORLD_SUNSTONE =
+            createKey("add_overworld_sunstone");
+
+    public static final ResourceKey<BiomeModifier> ADD_STARFIELDS_MOONSTONE =
+            createKey("add_starfields_moonstone");
+
+    public static final ResourceKey<BiomeModifier> ADD_STARFIELDS_SUNSTONE =
+            createKey("add_starfields_sunstone");
+
+    public static final ResourceKey<BiomeModifier> ADD_STARFIELDS_VEGETATION =
+            createKey("add_starfields_vegetation");
 
     public static void bootstrap(BootstapContext<BiomeModifier> context) {
-        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
         var biomes = context.lookup(Registries.BIOME);
+        var placedFeatures = context.lookup(Registries.PLACED_FEATURE);
 
-        // Solo en biomas del Overworld
-        context.register(ADD_MOONSTONE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        // Ores para Overworld (todos los biomas)
+        context.register(ADD_OVERWORLD_MOONSTONE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
                 biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
-                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.MOONSTONE_ORE_PLACED_KEY)),
-                GenerationStep.Decoration.UNDERGROUND_ORES));
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.OVERWORLD_MOONSTONE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES
+        ));
 
-        context.register(ADD_SUNSTONE_ORE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+        context.register(ADD_OVERWORLD_SUNSTONE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
                 biomes.getOrThrow(BiomeTags.IS_OVERWORLD),
-                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.SUNSTONE_ORE_PLACED_KEY)),
-                GenerationStep.Decoration.UNDERGROUND_ORES));
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.OVERWORLD_SUNSTONE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES
+        ));
+
+        // Ores específicos para Starfields
+        context.register(ADD_STARFIELDS_MOONSTONE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(ModBiomes.STARFIELDS_KEY)),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.STARFIELDS_MOONSTONE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES
+        ));
+
+        context.register(ADD_STARFIELDS_SUNSTONE, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(ModBiomes.STARFIELDS_KEY)),
+                HolderSet.direct(placedFeatures.getOrThrow(ModPlacedFeatures.STARFIELDS_SUNSTONE_PLACED_KEY)),
+                GenerationStep.Decoration.UNDERGROUND_ORES
+        ));
+
+        // Vegetación para Starfields
+        context.register(ADD_STARFIELDS_VEGETATION, new ForgeBiomeModifiers.AddFeaturesBiomeModifier(
+                HolderSet.direct(biomes.getOrThrow(ModBiomes.STARFIELDS_KEY)),
+                HolderSet.direct(
+                        placedFeatures.getOrThrow(ModPlacedFeatures.SOULWOOD_PLACED_KEY),
+                        placedFeatures.getOrThrow(ModPlacedFeatures.SOULSONG_FLOWER_PLACED_KEY)
+                ),
+                GenerationStep.Decoration.VEGETAL_DECORATION
+        ));
     }
 
-    private static ResourceKey<BiomeModifier> registerKey(String name) {
+    private static ResourceKey<BiomeModifier> createKey(String name) {
         return ResourceKey.create(ForgeRegistries.Keys.BIOME_MODIFIERS,
                 ResourceLocation.fromNamespaceAndPath(Stellarbound.MOD_ID, name));
     }
